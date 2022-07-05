32174700cec00a9f1a5310d255ca160f1bb3b126
Buyun Li <buyunli1997@gmail.com>
2022-07-04 17:08:30 -0700

    ssh test

--- template.js
+++
@@ -1,9 +0,0 @@
-function template(id, data) {
-    let str = document.getElementById('tpl-user').innerHTML()
-    let pattern = /{{\s*([a-zA-Z]+)\s*}}/
-    let patternResult = null
-    while (patternResult = pattern.exec(str)) {
-        str = str.replace(patternResult[0], data[patternResult[1]])
-    }
-    return str
-}
